e247c636f186b5adc61abeddac5dca3b0b599f43
Trillium Smith <spiteless@gmail.com>
2023-06-29 11:26:04 -0700

    fix: Prevent hover on unsupported devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ const plugin = require("tailwindcss/plugin")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
