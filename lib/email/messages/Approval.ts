595918de2dc0a7c2ba6d1d485796fbc37d5dd476
Trillium Smith <spiteless@gmail.com>
2023-07-01 19:52:12 -0700

    feat: Update email template - include session info

--- lib/email/messages/Approval.ts
+++ lib/email/messages/Approval.ts
@@ -9,19 +9,33 @@ export default function ApprovalEmail(props: EventProps & { approveUrl: string, 
 
   let body = `<div dir="ltr">`
   body += [
-    `<b>${name}</b> has requested a meeting on <b>${dateSummary}</b>, via <b>${location}</b>`,
+    `<b>${name}</b> has requested a meeting on <b>${dateSummary}</b>, at <b><a href="https://www.google.com/maps/preview?q=${encodeURI(location)}">${location}</a></b>`,
     `<br>`,
     `Their local timezone is ${timeZone}`,
-    `<br>`,
-    `<br>`,
+    `<br>`, 
     `<b><a href=${approveUrl}>Accept the meeting</a></b>`,
     `<br>`,
-    `<b><a href=${declineUrl}>Decline the meeting</a></b>`,
+    `<b><a href=${getDeclineUrl(props)}>Decline the meeting</a></b>`,
   ]
     .map((line) => `${LINE_PREFIX}${line}${LINE_SUFFIX}`)
     .join("")
 
+  body += "<br>"
+  body += detailsHelper(props, "<br>")
   body += `</div>`
 
   return { subject: SUBJECT, body }
+}
+
+function getDeclineUrl ({email, dateSummary, name}: {email: string, dateSummary: string, name: string}) {
+  let declineUrl = `mailto:${encodeURI(email)}?subject=${encodeURIComponent(
+    `Re: Your meeting request`
+  )}&body=${encodeURIComponent(
+    `Hi ${name},
+
+I just checked my calendar and it looks like ${dateSummary} won't work.
+
+Would you be able to meet at a different time?`
+  )}`
+  return declineUrl
 }
--- lib/email/messages/Confirmation.ts
+++ lib/email/messages/Confirmation.ts
@@ -3,25 +3,26 @@ import templates, { detailsHelper, signatureBlock } from "@/lib/messageTemplates
 const LINE_PREFIX = `<div class="gmail_default" style="font-family:arial,sans-serif">`
 const LINE_SUFFIX = `</div>`
 
-const SUBJECT = `Your meeting request`
+export default function ConfirmationEmail(props: any) {
+  const { dateSummary, name, price } = props
+  const SUBJECT = "Request for " + templates.eventSummary(props)
 
-export default function ConfirmationEmail({
-  dateSummary,
-}: {
-  dateSummary: string
-}) {
   let body = `<div dir="ltr">`
   body += [
-    `Hi there`,
+    `Hi ${name},`,
     `<br>`,
-    `Just confirming that your request for <b>${dateSummary}</b> has been received. 
-I'll review it as soon as I can and get back to you with a confirmation.`,
+    `Just wanted to let you know that your request for <b>${dateSummary}</b> for $<b>${price}</b> has been received I'll double check my calendar and get back to youn ASAP!`,
     `<br>`,
     `Thanks!`,
+    `<br>`,
+    `<b>Details:</b>`,
   ]
     .map((line) => `${LINE_PREFIX}${line}${LINE_SUFFIX}`)
     .join("")
 
+  body += detailsHelper(props, "<br>")
+  body += "<br>"
+  body += signatureBlock("<br>")
   body += `</div>`
 
   return { subject: SUBJECT, body }
