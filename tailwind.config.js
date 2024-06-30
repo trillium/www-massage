a2671ee9164505b3c3fe41dd0f0b4895d31e3928
Trillium Smith <spiteless@gmail.com>
2024-06-30 14:25:26 -0700

    feat: Add app directory in tailwind confing content

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ const plugin = require("tailwindcss/plugin")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
