a89cc43a68d5f4ea662e660c1b139162e07c1ae3
Trillium Smith <spiteless@gmail.com>
2024-07-05 15:01:22 -0700

    feat: Add in additional metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,39 @@ const public_sans = Public_Sans({
 })
 
 export const metadata: Metadata = {
+  metadataBase: new URL(siteMetadata.siteUrl),
   title: `Meet with ${process.env.NEXT_PUBLIC_OWNER_NAME ?? "me"}`,
+  openGraph: {
+    title: siteMetadata.title,
+    description: siteMetadata.description,
+    url: "./",
+    siteName: siteMetadata.title,
+    images: [siteMetadata.socialBanner],
+    locale: "en_US",
+    type: "website",
+  },
+  alternates: {
+    canonical: "./",
+    types: {
+      "application/rss+xml": `${siteMetadata.siteUrl}/feed.xml`,
+    },
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: siteMetadata.title,
+    card: "summary_large_image",
+    images: [siteMetadata.socialBanner],
+  },
 }
 
 export default function RootLayout({
