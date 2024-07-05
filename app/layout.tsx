11d538f5ac5cae0e9f261260b6f9446cebadb0a3
Trillium Smith <spiteless@gmail.com>
2024-07-05 14:40:32 -0700

    wip: Move <link> elements directly below <html>

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,18 +23,18 @@ export default function RootLayout({
     <html>
       <>
         <>
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
         </>
         <style jsx global>{`
           html {
