03dd85e1359f256d1116783116a8b77c210edc37
Trillium Smith <spiteless@gmail.com>
2024-07-05 13:01:27 -0700

    fixup: Remove NextResponse

--- app/api/request/route.ts
+++ app/api/request/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server"
+import { NextRequest } from "next/server"
 import { headers as nextHeaders } from "next/headers"
 import { IncomingMessage } from "http"
 
