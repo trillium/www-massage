31cac0ce0da4b09ea736efd9d68cff9b1ee24c6c
Trillium Smith <spiteless@gmail.com>
2024-07-06 20:50:57 -0700

    feat: Use fetch from /lib, remove extra imports in app/page

--- app/page.tsx
+++ app/page.tsx
@@ -1,62 +1,11 @@
 import type {
   InferGetServerSidePropsType,
 } from "next"
-import { z } from "zod"
 
 import ClientPage from "@/app/ClientPage"
-import {
-  ALLOWED_DURATIONS,
-  DEFAULT_APPOINTMENT_INTERVAL,
-  DEFAULT_DURATION,
-} from "@/config"
-import getBusyTimes from "@/lib/availability/getBusyTimes"
-import {
-  getDateRangeInterval,
-  mapDatesToStrings,
-} from "@/lib/availability/helpers"
-import Day from "@/lib/day"
+import { fetchData } from "@/lib/fetch/fetchData"
 
 export type PageProps = InferGetServerSidePropsType<typeof fetchData>
-
-async function fetchData({ searchParams }: { searchParams: URLSearchParams} ) {
-  const schema = z.object({
-    duration: z
-      .enum([...(ALLOWED_DURATIONS.map(String) as [string, ...string[]]), DEFAULT_APPOINTMENT_INTERVAL.toString()])
-      .optional()
-      .default(String(DEFAULT_DURATION))
-      .transform(Number),
-    timeZone: z.string().optional(),
-    selectedDate: z
-      .string()
-      .regex(/^\d{4}-\d{2}-\d{2}$/u)
-      .optional(),
-  })
-
-  const { duration, timeZone, selectedDate } = schema.parse(searchParams)
-
-  // Offer two weeks of availability.
-  const start = Day.todayWithOffset(0)
-  const end = Day.todayWithOffset(14)
-
-  const busy = await getBusyTimes(
-    getDateRangeInterval({
-      start,
-      end,
-      timeZone,
-    })
-  )
-
-  return {
-    props: {
-      start: start.toString(),
-      end: end.toString(),
-      busy: mapDatesToStrings(busy),
-      duration,
-      ...(timeZone && { timeZone }),
-      ...(selectedDate && { selectedDate }),
-    },
-  }
-}
 
 export const dynamic = "force-dynamic"
 
