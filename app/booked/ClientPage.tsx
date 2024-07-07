b62483936e87417adb9d3e0551cdf6da75667a7e
Trillium Smith <spiteless@gmail.com>
2024-07-07 10:31:58 -0700

    fix: Fix blank render for booked/ClientPage

--- app/booked/ClientPage.tsx
+++ app/booked/ClientPage.tsx
@@ -8,7 +8,13 @@ export default function Booked() {
   const url = searchParams.get("url")
 
   if (!url || typeof url !== "string") {
-    return
+    return (
+      <div className="py-8 sm:py-16 mx-auto max-w-xl">
+        <h1 className="text-3xl font-bold tracking-tight text-secondary-700">
+          There was an error with the url parameter.
+        </h1>
+      </div>
+    )
   }
   return (
     <div className="py-8 sm:py-16 mx-auto max-w-xl">
