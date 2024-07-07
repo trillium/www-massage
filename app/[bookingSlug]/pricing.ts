67beb9b22ab5ffb0657ceec756ef21b9cdd30979
Trillium Smith <spiteless@gmail.com>
2024-07-06 20:58:15 -0700

    feat: Create pricing file with booking slug names

---
+++ app/[bookingSlug]/pricing.ts
@@ -0,0 +1,46 @@
+export type Pricing = {
+  [key: number]: number;
+};
+
+export type Slugs = {
+  [category: string]: {
+    pricing: Pricing;
+  };
+};
+
+const slugs = {
+  app: {
+    pricing: {
+      60: 100,
+      90: 150,
+      120: 200,
+      150: 250,
+    },
+  },
+  book: {
+    pricing: {
+      60: 110,
+      90: 110 * 1.5,
+      120: 110 * 2,
+      150: 110 * 2.5,
+    },
+  },
+  spa: {
+    pricing: {
+      60: 120,
+      90: 120 * 1.5,
+      120: 120 * 2,
+      150: 120 * 2.5,
+    },
+  },
+  req: {
+    pricing: {
+      60: 130,
+      90: 130 * 1.5,
+      120: 130 * 2,
+      150: 130 * 2.5,
+    },
+  },
+}
+
+export default slugs
