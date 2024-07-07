7db825a1bed18f0782765357af833b52a128f014
Trillium Smith <spiteless@gmail.com>
2024-07-06 22:29:32 -0700

    chore: Clean up [bookingSlug], remove unused imports

--- app/[bookingSlug]/page.tsx
+++ app/[bookingSlug]/page.tsx
@@ -1,33 +1,23 @@
 import { redirect } from "next/navigation"
 import slugs from "./pricing"
 
-import type { InferGetServerSidePropsType } from "next"
-
 import ClientPage from "./ClientPage"
 import { fetchData } from "@/lib/fetch/fetchData"
-
-export const dynamic = "force-dynamic"
 
 export default async function Page({
   searchParams,
   params,
 }: {
   searchParams: URLSearchParams
-  params: { bookingPageId: string}
+  params: { bookingSlug: string }
 }) {
-  // const { params } = props
-  const { bookingPageId } = params
-  const { pricing } = slugs[bookingPageId as keyof typeof slugs] ?? false
+  const { bookingSlug } = params
+  const { pricing } = slugs[bookingSlug as keyof typeof slugs] ?? false
 
   if (!pricing) {
     return redirect("/")
   }
 
   const { props } = await fetchData({ searchParams })
-  return (
-    <>
-      
-      <ClientPage {...props} pricing={pricing} />
-    </>
-  )
+  return <ClientPage {...props} pricing={pricing} />
 }
