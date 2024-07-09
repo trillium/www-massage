721eb97a3a18945577278d990d0e9961ce14e176
Trillium Smith <spiteless@gmail.com>
2024-07-09 13:48:55 -0700

    feat: Update Subject and Message body for ClientRequestEmail

--- lib/email/messages/Confirmation.ts
+++ lib/email/messages/Confirmation.ts
@@ -9,14 +9,28 @@ export default function ClientRequestEmail({
   name,
   dateSummary,
 }: Omit<EmailProps, "approveUrl">) {
+  const SUBJECT = `Massage Session Request ${price}, ${duration}`
+
   let body = `<div dir="ltr">`
   body += [
-    `Hi there`,
+    `Hi ${name || "there"}`,
     `<br>`,
-    `Just confirming that your request for <b>${dateSummary}</b> has been received. 
-I'll review it as soon as I can and get back to you with a confirmation.`,
+    `Just letting you know I received your appointment request!`,
+    `<br>`,
+    `<b>Date:</b> ${dateSummary}`,
+    `<br>`,
+    `<b>Duration:</b> ${duration} minutes`,
+    `<br>`,
+    `<b>Location:</b> ${location}`,
+    `<br>`,
+    `<b>Price:</b> ${price}`,
+    `<br>`,
+    `<br>`,
+    `I'll review it as soon as I can and get back to you with a confirmation.`,
     `<br>`,
     `Thanks!`,
+    `<br>`,
+    `Trillium Smith`,
   ]
     .map((line) => `${LINE_PREFIX}${line}${LINE_SUFFIX}`)
     .join("")
