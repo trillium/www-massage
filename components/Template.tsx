a9c6b07f439e4a8f850ad5a311cd04cb09f229b9
Trillium Smith <spiteless@gmail.com>
2024-07-20 13:04:29 -0700

    feat: Reduce Template y-axis padding

--- components/Template.tsx
+++ components/Template.tsx
@@ -5,7 +5,7 @@ interface TemplateProps {
 
 export default function Template({ title, text }: TemplateProps) {
   return (
-    <div className="pt-8 sm:pt-16 pb-4 sm:pb-16">
+    <div className="py-4">
       <h1 className="text-3xl font-bold tracking-tight text-secondary-700 dark:text-secondary-400 sm:text-5xl">
         {title}
       </h1>
