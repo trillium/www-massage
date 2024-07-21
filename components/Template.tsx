428b9f8966091608db163af89d2c628b39e5a465
Trillium Smith <spiteless@gmail.com>
2024-07-21 16:12:28 -0700

    feat: Remove required on prop {text} in Template

--- components/Template.tsx
+++ components/Template.tsx
@@ -1,6 +1,6 @@
 interface TemplateProps {
-  title: string,
-  text: string
+  title: string
+  text?: string
 }
 
 export default function Template({ title, text }: TemplateProps) {
@@ -9,9 +9,11 @@ export default function Template({ title, text }: TemplateProps) {
       <h1 className="text-3xl font-bold tracking-tight text-primary-500 dark:text-primary-400 sm:text-5xl">
         {title}
       </h1>
-      <p className="mt-2 sm:mt-6 sm:text-xl text-gray-800 dark:text-gray-100 font-medium">
-        {text}
-      </p>
+      {text && (
+        <p className="mt-2 sm:mt-6 sm:text-xl text-gray-800 dark:text-gray-100 font-medium">
+          {text}
+        </p>
+      )}
     </div>
   )
 }
