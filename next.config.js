a4b3027ebdeeebe21c35cf9a0f349a9a00454626
Trillium Smith <spiteless@gmail.com>
2024-07-20 18:10:29 -0700

    feat: Allow next to render svgs on import

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ module.exports = {
   experimental: {
     swcMinify: true,
   },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    })
+    return config
+  },
 }
