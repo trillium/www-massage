8936b9d0997842a867733674cbe8b5ee325ade73
Trillium Smith <spiteless@gmail.com>
2024-07-26 11:08:34 -0700

    feat: Add /faq to headerNavLinks

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/book', title: 'Book Appointment' },
   { href: '/about', title: 'About' },
+  { href: '/faq', title: 'FAQ' },
 ]
 
 export default headerNavLinks
