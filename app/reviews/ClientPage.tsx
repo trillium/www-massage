315223dc04524ad23a937ec5e5a4836d6b0d4764
Trillium Smith <spiteless@gmail.com>
2024-08-19 03:15:19 -0700

    feat: Create /reviews page

---
+++ app/reviews/ClientPage.tsx
@@ -0,0 +1,12 @@
+"use client"
+import ReviewCard from "@/components/ReviewCard"
+import BookSessionButton from "@/components/BookSessionButton"
+
+export default function About({}) {
+  return (
+    <div className="flex flex-col items-center">
+      <ReviewCard />
+      <BookSessionButton title="Book a Session!" href="/book" />
+    </div>
+  )
+}
