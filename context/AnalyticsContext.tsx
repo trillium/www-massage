58f024e3e4dab4e908636c430a1d8fef39124595
Trillium Smith <spiteless@gmail.com>
2024-08-20 11:09:41 -0700

    style: Lint file AnalyticsContext

--- context/AnalyticsContext.tsx
+++ context/AnalyticsContext.tsx
@@ -7,9 +7,9 @@ import { PostHogProvider } from "posthog-js/react"
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
     api_host: "/ingest",
-    ui_host: 'https://us.posthog.com',
+    ui_host: "https://us.posthog.com",
     person_profiles: "always", // or 'always' to create profiles for anonymous users as well
-    custom_campaign_params: ["ref"]
+    custom_campaign_params: ["ref"],
   })
 }
 
