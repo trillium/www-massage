c4b32d5111dd74306fef1fc50112078bd3764a95
Trillium Smith <spiteless@gmail.com>
2024-09-19 15:20:51 -0700

    feat: Pass additional props to templates.eventDescription

--- lib/messageTemplates/templates.ts
+++ lib/messageTemplates/templates.ts
@@ -25,27 +25,30 @@ function eventSummary({
 function eventDescription({
   start,
   end,
-  summary,
+  phone,
+  duration,
   email,
   location,
   firstName,
+  lastName,
   eventBaseString,
   eventMemberString,
   eventContainerString,
 }: Partial<AppointmentProps>) {
   let output = "Thanks for booking!"
   output += "\n\n"
-  output += `<b>Name</b>: ${firstName}\n`
+  output += `<b>Name</b>: ${firstName} ${lastName}\n`
   output += `<b>Date</b>: ${new Date(start || "").toDateString()}\n`
   output += `<b>Start</b>: ${new Date(start || "").toLocaleTimeString()}\n`
   output += `<b>End</b>: ${new Date(end || "").toLocaleTimeString()}\n`
-  output += `<b>Summary</b>: ${summary}\n`
+  output += `<b>Duration</b>: ${duration}\n`
   output += `<b>Email</b>: ${email}\n`
+  output += `<b>Phone</b>: ${phone}\n`
   output += `<b>Location</b>: ${location}\n`
   output += "\n\n"
   output += "Trillium Smith, LMT"
   output += "\n"
-  output += `<a href="www.trilliummassage.la">www.trilliummassage.la</a>`
+  output += `<a href="www.trilliummassage.la">www.trilliummassage.la</a>\n`
 
   if (eventBaseString) {
     output += "\n"
