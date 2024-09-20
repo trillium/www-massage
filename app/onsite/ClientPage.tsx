dc5e2b6b94a31598ebb661e838bdf0f2d78c92c4
Trillium Smith <spiteless@gmail.com>
2024-09-20 12:44:20 -0700

    feat: Create scaffolding for /onsite from /app page, ClientPage

---
+++ app/onsite/ClientPage.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import type { InferGetServerSidePropsType } from "next"
+
+import AvailabilityPicker from "@/components/availability/AvailabilityPicker"
+import { PricingWrapper } from "@/components/availability/PricingWrapper"
+import { DEFAULT_PRICING } from "@/config"
+
+import PageProps from "@/app/page"
+
+const pricing = DEFAULT_PRICING
+
+// Need to refactor fetchData so it's easier to extend to other pages
+
+function Page({
+  start,
+  end,
+  busy,
+  selectedDate,
+  duration,
+}: InferGetServerSidePropsType<typeof PageProps>) {
+  const { slots, pickerProps } = PricingWrapper({
+    start,
+    end,
+    busy,
+    pricing,
+    selectedDate,
+    duration,
+  })
+
+  return (
+    <>
+      <AvailabilityPicker slots={slots} pickerProps={pickerProps} />
+    </>
+  )
+}
+
+export default Page
