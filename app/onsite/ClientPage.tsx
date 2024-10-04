843c8650f4fd4ccfaca08f5777594151d75a1f36
Trillium Smith <spiteless@gmail.com>
2024-10-04 12:32:33 -0700

    wip: Put up /onsite despite work in progress status

--- app/onsite/ClientPage.tsx
+++ app/onsite/ClientPage.tsx
@@ -8,7 +8,9 @@ import { DEFAULT_PRICING } from "@/config"
 
 import PageProps from "@/app/page"
 import { AllowedDurationsType } from "@/lib/types"
-import { FormEvent, useState } from "react"
+import { FormEvent, useEffect, useState } from "react"
+
+import { dumpData } from "@/lib/dataLoading"
 
 const pricing = DEFAULT_PRICING
 
@@ -26,9 +28,11 @@ const allowedDurations: AllowedDurationsType = [
 ]
 
 type StateType = {
+  eventContainerString: string
   allowedDurations: number[]
   eventName: string
-  sessionDuration: string
+  sessionDuration?: string
+  pricing?: { [key: number]: number };
 }
 
 function ClientPage({
@@ -50,8 +54,10 @@ function ClientPage({
 
   const [state, setState] = useState<StateType>({
     eventName: "",
-    sessionDuration: "",
-    allowedDurations: []
+    // sessionDuration: "",  
+    eventContainerString: "__EVENT__",
+    allowedDurations: [],
+    pricing: { 15: 120 / 4, 30: 120 / 2 }
   })
 
   const [pathString, setPathString] = useState("");
@@ -85,17 +91,21 @@ function ClientPage({
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      const newPathString =
-        window.location.origin +
-        "/" +
-        state.eventName
+      const sanitizedName = state.eventName
           .replace(/\s|-/g, "_")
           .toLowerCase()
           .replace(/[^a-z0-9_]/g, "");
+      const newPathString =
+        window.location.origin +
+        "/" +
+        sanitizedName
       setPathString(newPathString);
+      setState({ ...state, eventContainerString: sanitizedName + "__EVENT__"})
     }
   }, [state.eventName]);
-  
+
+  console.log(dumpData(state))
+
   return (
     <>
       <div className="w-full flex justify-center items-center align-middle">
@@ -104,10 +114,47 @@ function ClientPage({
         </h2>
       </div>
       <form onSubmit={handleSubmit}>
-      <input type="hidden" readOnly name="test1" value={"Test1"} />
-      <input type="hidden" readOnly name="test2" value={"Test2"} />
         <ol>
-          <li>Choose url</li>
+          <label
+            htmlFor="eventName"
+            className="block text-base font-medium text-gray-900 dark:text-gray-100">
+            Booking Link Name
+          </label>
+          <input
+            aria-label="Comment"
+            type="text"
+            name="eventName"
+            id="eventName"
+            value={state.eventName}
+            className="pl-2 py-1 block w-full border-0 p-0 text-gray-900 dark:text-gray-100 placeholder:text-gray-400 focus:ring-0 sm:text-base sm:leading-6 mb-1"
+            placeholder="e.g., WeWork Playa Vista"
+            onChange={formOnChange}
+            maxLength={300}
+          />
+
+          <label
+            htmlFor="sessionDuration"
+            className="block text-base font-medium text-gray-900 dark:text-gray-100">
+            How long should sessions be?
+          </label>
+          <div className="flex flex-col space-y-2">
+            {possibleDurations.map((duration) => (
+
+              <div className="flex items-center"
+                key={duration}
+              >
+                <input
+                  checked={state.allowedDurations.includes(duration)}
+                  id="checked-checkbox" type="checkbox" value={duration}
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-primary-500 dark:focus:ring-primary-400 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  onChange={formCheckboxOnChange}
+                />
+                <label htmlFor="checked-checkbox" className="ms-2 font-medium">{duration} minutes</label>
+              </div>
+
+            ))}
+          </div>
+
           <li>Determine How Many Sessions</li>
           <li>Allow users to book single or multi duration</li>
           <li>User payment vs company payment</li>
@@ -117,6 +164,8 @@ function ClientPage({
       </form>
       <AvailabilityPicker slots={slots} pickerProps={pickerProps} />
       <pre>{JSON.stringify(state, null, 2)}</pre>
+      <pre className="border m-4 p-2 rounded-md border-black" >{dumpData(state)}</pre>
+      <textarea value={dumpData(state)} className="border m-4 p-2 rounded-md border-black w-full"/>
     </>
   )
 }
