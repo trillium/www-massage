aeccc053de5c626dffe817acb143392d7d57bd42
Trillium Smith <spiteless@gmail.com>
2024-11-26 12:58:58 -0800

    feat: Change DurationPicker to use radiobutton inputs

--- components/availability/controls/DurationPicker.tsx
+++ components/availability/controls/DurationPicker.tsx
@@ -14,37 +14,48 @@ export default function DurationPicker({ title, allowedDurations }: durationProp
   const { duration } = useReduxAvailability()
   const dispatchRedux = useAppDispatch()
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatchRedux(setDuration(Number(event.target.value)))
+  }
+
+
   return (
-    <div>
-      <label
-        htmlFor="duration"
-        className="block text-sm font-medium leading-0 text-gray-900 dark:text-gray-100">
+    <fieldset>
+      <legend className="block text-sm font-medium leading-0 text-gray-900 dark:text-gray-100">
         {title || "Duration"}
-      </label>
-      <div className="isolate inline-flex rounded-md shadow-sm mt-1 h-9">
+      </legend>
+      <div className="isolate inline-flex rounded-md shadow-sm mt-1 h-9 active:ring-2 focus-within:ring-2 focus-within:ring-primary-500 active:ring-primary-500">
         {ALLOWED_DURATIONS.map((theDuration, i) => (
-          <button
-            key={theDuration}
-            onClick={() => {
-              dispatchRedux(setDuration(theDuration))
-            }}
-            type="button"
-            className={clsx(
-              "relative inline-flex items-center px-3 py-2 text-sm font-semibold ring-1 ring-inset focus:z-10 outline-primary-600",
-              {
-                "rounded-l-md": i === 0,
-                "rounded-r-md": i === ALLOWED_DURATIONS.length - 1,
-                "-ml-px": i > 0,
-                "bg-white text-gray-900 ring-gray-300 hover:bg-gray-200":
-                  theDuration !== duration,
-                "bg-primary-500 text-white ring-primary-400 shadow-inner shadow-primary-900":
-                  theDuration === duration,
-              }
-            )}>
-            {theDuration}m
-          </button>
+          <div key={theDuration} className="flex items-center">
+            <input
+              id={`duration-${theDuration}`}
+              name="duration"
+              type="radio"
+              value={theDuration}
+              checked={duration === theDuration}
+              onChange={handleChange}
+              className="sr-only"
+            />
+            <label
+              htmlFor={`duration-${theDuration}`}
+              className={clsx(
+                "relative inline-flex items-center px-3 py-2 text-sm font-semibold ring-1 ring-inset focus:z-10 outline-primary-600",
+                {
+                  "rounded-l-md": i === 0,
+                  "rounded-r-md": i === ALLOWED_DURATIONS.length - 1,
+                  "-ml-px": i > 0,
+                  "bg-white text-gray-900 ring-gray-300 hover:bg-gray-200":
+                    theDuration !== duration,
+                  "bg-primary-500 text-white ring-primary-400 shadow-inner shadow-primary-900":
+                    theDuration === duration,
+                }
+              )}
+            >
+              {theDuration}m
+            </label>
+          </div>
         ))}
       </div>
-    </div>
+    </fieldset >
   )
 }
